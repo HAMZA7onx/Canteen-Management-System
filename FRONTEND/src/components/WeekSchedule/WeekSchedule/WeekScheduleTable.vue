94e0f5ab10f71d4494144381af6989b4c1240ae6
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-06-28 11:53:11 +0200

    fx2

--- FRONTEND/src/components/WeekSchedule/WeekSchedule/WeekScheduleTable.vue
+++ FRONTEND/src/components/WeekSchedule/WeekSchedule/WeekScheduleTable.vue
@@ -52,7 +52,7 @@
                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
                   @click="openEditModal(weekSchedule)"
                 >
-                  Edit
+                  Show
                 </button>
                 <button
                   class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
