1f7c82c2a24a99807915e2c4dd062736115554c7
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-07-07 21:13:54 +0200

    fixing issues 2

--- FRONTEND/src/components/WeekSchedule/WeekSchedule/WideModal.vue
+++ FRONTEND/src/components/WeekSchedule/WeekSchedule/WideModal.vue
@@ -1,7 +1,10 @@
 
 
   <template>
-    <slot></slot>
+    <div class="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full flex items-start justify-center p-4 z-50">
+      <slot></slot>
+
+    </div>
   </template>
   
   
