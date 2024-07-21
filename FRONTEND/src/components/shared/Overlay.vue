f2f645f7daa322d323cf6ce6d3dfe4a5019710d0
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-07-21 08:31:08 +0200

    enhancing admins dash UX

--- FRONTEND/src/components/shared/Overlay.vue
+++ FRONTEND/src/components/shared/Overlay.vue
@@ -1,6 +1,6 @@
 <!-- Overlay.vue -->
 <template>
-<div class="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full flex items-start justify-center p-4 z-50">
+<div class="fixed z-50 inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full flex items-start justify-center p-4 ">
     <slot></slot>
 </div>
 </template>
