9e6ec24dd24e3f35237d1ff07d2753833a5dbfd7
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-08-07 14:44:00 +0200

    fixing stuffs

--- FRONTEND/src/components/AdminBadge/MarkAsLostModal.vue
+++ FRONTEND/src/components/AdminBadge/MarkAsLostModal.vue
@@ -1,16 +1,14 @@
 <template>
-      <h2 class="text-2xl sm:text-3xl font-bold mb-6 text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-indigo-600 dark:from-purple-400 dark:to-indigo-400">Edit Badge</h2>
-
       <form @submit.prevent="handleSubmit">
         <div v-if="badge.status === 'assigned'" class="text-center">
-          <p class="text-gray-700 dark:text-gray-300 mb-6">Are you sure you want to mark this RFID as lost?</p>
+          <p class="text-gray-700 dark:text-gray-300 mb-6">Etes-vous sûr de vouloir marquer cette RFID comme perdue ?</p>
           
           <button
             type="button"
             class="w-full bg-gradient-to-r from-red-500 to-red-600 text-white px-4 py-3 rounded-lg hover:from-red-600 hover:to-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-all duration-300 transform hover:scale-105"
             @click="markAsLost"
           >
-            Mark as Lost
+          Marquer comme perdu
           </button>
         </div>
       </form>
